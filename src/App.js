83a93a2bec9d679f8db56d2afb7c2f15b0ae04ec
YanLinAung123 <yaung2444@gmail.com>
2024-01-22 21:44:28 +0630

    step-6 using props

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-function Item(){
-  return <li>Content</li>;
+function Item(props){
+  return <li>{props.name}, ${props.price}</li>;
 }
 export default function App(){
   return (
     <div>
       <h1>Hello React</h1>
       <ul>
-        <Item />
-        <Item />
+        <Item name="Apple" price="0.99" />
+        <Item name="Orange" price="0.89" />
       </ul>
     </div>
     );
