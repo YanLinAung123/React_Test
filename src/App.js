057a81b30863e7bbb32e03973077afd9483cb875
YanLinAung123 <yaung2444@gmail.com>
2024-01-22 21:50:24 +0630

    step-7 useState

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
+import {useState} from "react";
 function Item(props){
   return <li>{props.name}, ${props.price}</li>;
 }
 export default function App(){
+  const [data,setData] =useState([
+    {id:1,name:"Apple" , price:0.99},
+    {id:2,name:"Orange" , price:0.89},
+  ]);
   return (
     <div>
       <h1>Hello React</h1>
       <ul>
-        <Item name="Apple" price="0.99" />
-        <Item name="Orange" price="0.89" />
+        {data.map(i=>(
+          <Item name={i.name} price={i.price} />
+          ))}
       </ul>
     </div>
     );
