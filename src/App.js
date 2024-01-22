6ee16d1a5121034232719decef92f6e6a519f140
YanLinAung123 <yaung2444@gmail.com>
2024-01-22 21:42:14 +0630

    step-5 using component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
+function Item(){
+  return <li>Content</li>;
+}
 export default function App(){
   return (
     <div>
       <h1>Hello React</h1>
       <ul>
-        <li>Item One</li>
-        <li>Item Two</li>
+        <Item />
+        <Item />
       </ul>
     </div>
     );
